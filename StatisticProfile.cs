c04e7ce121f1735949bbc17a9c661388161d0e04
erdopfi <kikikneissl@gmail.com>
2022-08-10 21:20:00 +0200

    Threads, time and dates
    Added thread support, finally made the refresh time and -amount useful and added dates to track statistics over time.

--- StatisticProfile.cs
+++ StatisticProfile.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
+using System.Globalization;
 using Newtonsoft.Json.Linq;
 
 namespace MinecraftDB
@@ -10,6 +12,10 @@ namespace MinecraftDB
         public readonly string WorldName;
         public readonly JObject Stats;
         
+        private const string SelectQuery = 
+            "select sum(value) from Stat where category = @category and world = @world and mod = @mod and id = @id and uuid = @uuid";
+        private const string InsertQuery = 
+            "insert into Stat values(@category, @world, @mod, @id, @uuid, @date, @value)";
 
         public StatisticProfile(string uuid, string worldName, JObject stats)
         {
@@ -20,6 +26,7 @@ namespace MinecraftDB
 
         public void WriteDatabase(SqlConnection sqlConnection)
         {
+            string dateString = DateTime.Now.ToString(CultureInfo.InvariantCulture);
             foreach (var categoryProperty in Stats.Properties())
             {
                 var categoryValue = categoryProperty.Value;
@@ -34,14 +41,47 @@ namespace MinecraftDB
                     var mod = idModString[0];
                     var id = idModString[1];
                     var value = idProperty.Value.ToObject<int>();
+                    
+                    var selectCommand = new SqlCommand(SelectQuery, sqlConnection);
+                    selectCommand.Parameters.Add(new SqlParameter("@category", SqlDbType.NChar, 100) {Value = category});
+                    selectCommand.Parameters.Add(new SqlParameter("@world", SqlDbType.NChar, 50){Value = WorldName});
+                    selectCommand.Parameters.Add(new SqlParameter("@mod", SqlDbType.NChar, 50){Value = mod});
+                    selectCommand.Parameters.Add(new SqlParameter("@id", SqlDbType.NChar, 50){Value = id});
+                    selectCommand.Parameters.Add(new SqlParameter("@uuid", SqlDbType.NChar, 36){Value = Uuid});
+                    var dbValue = 0;
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            if (reader[0] is DBNull)
+                                dbValue = 0;
+                            else
+                                dbValue = (int) reader[0];
 
-                    Console.WriteLine(Uuid + " - " + category + " - " + idModString + " - " + value);
-                    string deleteQuery =
-                        $"delete from Stat where category='{category}' and world='{WorldName}' and mod='{mod}' and id='{id}' and uuid='{Uuid}'";
-                    var deleteCmd = new SqlCommand(deleteQuery, sqlConnection);
-                    string insertQuery = $"insert into Stat values('{category}', '{WorldName}', '{mod}', '{id}', '{Uuid}', {value})";
-                    var insertCmd = new SqlCommand(insertQuery, sqlConnection);
-                    deleteCmd.ExecuteNonQuery();
+                            
+                            if (value - dbValue == 0)
+                            {
+                                continue;
+                            }
+                        }
+                    }
+
+                    var deltaValue = value - dbValue;
+                    
+                    //Console.WriteLine($"{value} - {dbValue} - {value - deltaValue}");
+
+                    Console.WriteLine(WorldName + " - " + Uuid + " - " + dateString + " - " + category + " - " + idProperty.Name + " - " + deltaValue);
+                    var insertCmd = new SqlCommand(InsertQuery, sqlConnection);
+
+                    insertCmd.Parameters.Add(new SqlParameter("@category", SqlDbType.NChar, 100) {Value = category});
+                    insertCmd.Parameters.Add(new SqlParameter("@world", SqlDbType.NChar, 50){Value = WorldName});
+                    insertCmd.Parameters.Add(new SqlParameter("@mod", SqlDbType.NChar, 50){Value = mod});
+                    insertCmd.Parameters.Add(new SqlParameter("@id", SqlDbType.NChar, 50){Value = id});
+                    insertCmd.Parameters.Add(new SqlParameter("@uuid", SqlDbType.NChar, 36){Value = Uuid});
+                    insertCmd.Parameters.Add(new SqlParameter("@date", SqlDbType.SmallDateTime, 50){Value = dateString});
+                    insertCmd.Parameters.Add(new SqlParameter("@value", SqlDbType.Int, 0){Value = deltaValue});
+
+                    insertCmd.Prepare();
                     insertCmd.ExecuteNonQuery();
                 }
             }
--- WorldDatabase.cs
+++ WorldDatabase.cs
@@ -4,20 +4,19 @@ using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
 namespace MinecraftDB
 {
     public class WorldDatabase
     {
-        private const string ConnectionString = "Data Source = DESKTOP-ME8DQVU;Initial Catalog=MinecraftDB;Integrated Security=true";
-
         private readonly string _worldPathDirectory;
         private readonly string _worldName;
         private int _refreshAmount;
-        private int _refreshCooldown;
+        private int _refreshCooldownInSeconds;
         private int _statisticsVersion;
-        
+
         private readonly SqlConnection _con;
 
         public WorldDatabase(WorldConfiguration configuration)
@@ -25,11 +24,8 @@ namespace MinecraftDB
             _worldPathDirectory = configuration.WorldPathDirectory;
             _worldName = _worldPathDirectory.Split("\\")[^1];
             _refreshAmount = configuration.RefreshAmount;
-            _refreshCooldown = configuration.RefreshCooldown;
+            _refreshCooldownInSeconds = configuration.RefreshCooldown;
             _statisticsVersion = configuration.StatisticsVersion;
-            
-            Console.WriteLine(_worldName);
-            Console.WriteLine(_worldPathDirectory);
 
             string worldIdFile = _worldPathDirectory + "\\world.id";
             if (File.Exists(worldIdFile))
@@ -46,12 +42,12 @@ namespace MinecraftDB
             _con.Open();
         }
 
-        public WorldDatabase(string worldPathDirectory, int refreshAmount, int refreshCooldown, int statisticsVersion, string databaseConnection)
+        public WorldDatabase(string worldPathDirectory, int refreshAmount, int refreshCooldownInSeconds, int statisticsVersion, string databaseConnection)
         {
             _worldPathDirectory = worldPathDirectory;
             _worldName = worldPathDirectory.Split("\\")[^1];
             _refreshAmount = refreshAmount;
-            _refreshCooldown = refreshCooldown;
+            _refreshCooldownInSeconds = refreshCooldownInSeconds;
             _statisticsVersion = statisticsVersion;
 
             _con = new SqlConnection(databaseConnection);
@@ -60,17 +56,35 @@ namespace MinecraftDB
 
         public void Start()
         {
-            var profiles = StatsCreator.CreateAll(_worldPathDirectory, _worldName);
-            
-            foreach (var profile in profiles)
-            {
-                profile.WriteDatabase(_con);
-            }
+            Thread t = new(ProfileWriteThread);
+            t.Start();
         }
 
         public void Stop()
         {
-            throw new NotImplementedException();
+            _refreshAmount = 0;
+            Console.WriteLine("Stopped world " + _worldName);
+        }
+
+        private void ProfileWriteThread()
+        {
+            while (_refreshAmount != 0)
+            {
+                var profiles = StatsCreator.CreateAll(_worldPathDirectory, _worldName);
+
+                foreach (var profile in profiles)
+                {
+                    profile.WriteDatabase(_con);
+                }
+
+                string consoleOutput = _refreshAmount < 0
+                    ? $"{_worldName} is waiting for {_refreshCooldownInSeconds} seconds - Infinite repeats left"
+                    : $"{_worldName} is waiting for {_refreshCooldownInSeconds} seconds - {_refreshAmount} repeats left";
+                Console.WriteLine(consoleOutput);
+                _refreshAmount--;
+                
+                Thread.Sleep(_refreshCooldownInSeconds * 1000);
+            }
         }
     }
 }
